821f736eda03ce6ed0645068ef0a0f9a58a69cc0
Daniel Adames <danieladames24@gmail.com>
2023-06-30 01:34:57 +0200

    feat: complete starting design

---
+++ calendario-piso/components/calendar.vue
@@ -0,0 +1,32 @@
+<script setup>
+const { month, year } = useDate(new Date());
+const tests = computed(() => {
+  const a = new Array(31);
+  const array = [];
+  for (let i = 0; i < a.length; i++) {
+    array.push(i + 1);
+  }
+  return array;
+});
+</script>
+
+<template>
+  <div>
+    <span
+      class="flex justify-center text-4xl bg-red-200 border-solid border-t border-r border-l border-black mx-5"
+      >{{ month }} {{ year }}</span
+    >
+    <div class="px-5">
+      <div class="grid grid-cols-7 gap-px pb-0.5">
+        <div v-for="day in days">
+          <DayOfTheWeek :day-of-the-week="day" />
+        </div>
+      </div>
+      <div class="grid grid-cols-7 gap-px">
+        <div v-for="test in tests">
+          <DayOfTheMonth :dayNumber="test" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ calendario-piso/pages/index.vue
@@ -0,0 +1,7 @@
+<script ts></script>
+
+<template>
+  <div class="bg-teal-100 h-screen grid items-center content-center">
+    <Calendar />
+  </div>
+</template>
