d170dfe07d2f18bc2a2356fdf74ea34e58fa22f6
Daniel Adames <danieladames24@gmail.com>
2023-06-30 21:39:24 +0200

    feat: implement new days info logic

--- calendario-piso/components/dayOfTheMonth.vue
+++ calendario-piso/components/dayOfTheMonth.vue
@@ -1,9 +1,16 @@
 <script setup>
-defineProps(['dayNumber']);
+const props = defineProps(['dayNumber']);
+
+const styling = computed(() => {
+  if (!props.dayNumber) {
+    return { 'background-color': '#D3D3D3' };
+  }
+  return {};
+});
 </script>
 
 <template>
-  <div class="p-4 border-solid border-2 border-black h-28">
-    {{ dayNumber }}
+  <div class="p-4 border-solid border-2 border-black h-28" :style="styling">
+    <span v-if="dayNumber">{{ dayNumber }}</span>
   </div>
 </template>
