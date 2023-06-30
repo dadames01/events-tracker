01bf2200b7ad57b824cc22195959b626e1927112
Daniel Adames <danieladames24@gmail.com>
2023-06-30 21:39:11 +0200

    feat: get days info

--- calendario-piso/composables/use-date.ts
+++ calendario-piso/composables/use-date.ts
@@ -1,5 +1,35 @@
 export const useDate = (currentDate: Date) => {
-  const month = months[currentDate.getMonth()];
-  const year = currentDate.getFullYear();
-  return { month, year };
+  const currentMonth = currentDate.getMonth();
+  const currentYear = currentDate.getFullYear();
+  const firstWeekDay = getStartingWeekDay(currentYear, currentMonth);
+  const lastDayOfMonth = getLastDayOfMonth(currentMonth, currentYear);
+
+  const currentMonthString = months[currentMonth];
+
+  const daysInfo = computed(() => {
+    const array = [];
+    let counter = 1;
+    let foundStartingDay = false;
+
+    while (counter < lastDayOfMonth) {
+      for (let i = 0; i < 7; i++) {
+        if (firstWeekDay === i && counter === 1) {
+          foundStartingDay = true;
+        }
+        if (foundStartingDay && counter <= lastDayOfMonth) {
+          array.push({ dayNumber: counter });
+          counter++;
+        } else {
+          array.push({ dayNumber: 0 });
+        }
+      }
+    }
+    return array;
+  });
+
+  return {
+    month: currentMonthString,
+    year: currentYear,
+    daysInfo,
+  };
 };
