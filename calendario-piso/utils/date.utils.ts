19b07dec65b360944de4b0d341f27f8146c5fa91
Daniel Adames <danieladames24@gmail.com>
2023-06-30 21:38:55 +0200

    feat: add new utils

--- calendario-piso/utils/date.utils.ts
+++ calendario-piso/utils/date.utils.ts
@@ -14,3 +14,20 @@ export const months = [
 ];
 
 export const days = ['DOM', 'LUN', 'MAR', 'MIE', 'JUE', 'VIE', 'SAB'];
+
+export const getStartingWeekDay = (
+  currentYear: number,
+  currentMonth: number
+) => {
+  return new Date(currentYear, currentMonth, 1).getDay();
+};
+
+export const getLastDayOfMonth = (
+  currentMonth: number,
+  currentYear: number
+) => {
+  if (currentMonth === 11) {
+    return new Date(currentYear + 1, currentMonth + 1, 0).getDate();
+  }
+  return new Date(currentYear, currentMonth + 1, 0).getDate();
+};
